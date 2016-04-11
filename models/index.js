d7ebfb1426de2821f274b5aa3b30d594df69d1df
Sam <samuel1008@gmail.com>
2016-04-11 11:16:36 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -3,3 +3,7 @@ mongoose.connect('mongodb://localhost/project_01');
 
 module.exports.Snippet = require("./snippet");
 module.exports.User = require("./user");
+
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
