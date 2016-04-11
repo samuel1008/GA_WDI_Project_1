dd938ccacfad1de2290f96b6549f9b03925b5634
Sam <samuel1008@gmail.com>
2016-04-11 11:27:47 -0700

    rearranged index.js to see if it'll work on heroku

--- models/index.js
+++ models/index.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/project_01');
+mongoose.connect( process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  'mongodb://localhost/project_01');
 
 module.exports.Snippet = require("./snippet");
 module.exports.User = require("./user");
-
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
-                  "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
